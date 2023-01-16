08513a5a9d645740444c2911ed36da76dd88f802
bhupeshjoshi7 <121234853+bhupeshjoshi7@users.noreply.github.com>
2023-01-16 15:32:03 +0530

    Update q7.py

--- q7.py
+++ q7.py
@@ -1,11 +1,40 @@
-#str = "hare krishna"
-#print(str[::-1])
-n=int(input("Enter the number of rows"))
-for i in range(0,n):
-    for j in range(0,i+1):
-        print("*",end='')
-    print()    
-for i in range(0,n):
-    for j in range(0,n-1-i):
-        print("*",end='')
-    print()          
+n=int(input("Enter number of elements N in fibonacci series:\n[N must be positive Integer]: N="))
+
+if n<=0 :
+    print("\nError\nNumber of elements in fibonacci series must be integer and greater than zero.")
+
+else:
+   
+    if n == 1:
+        print("\nThe fibonacci series with 1 element is shown below\n[1]")
+        print("\nAverage of given fibonacci series is", 1)
+
+    elif n == 2:
+        print("\nThe fibonacci series with 2 element is shown below\n[1,1]")
+        print("\nAverage of given fibonacci series is", 1)
+    
+    else:
+        
+        list1 = [1, 1]
+        
+        a = 1
+        b = 1
+        
+        for i in range(n - 2):
+            s = a + b
+            list1.append(s)
+            a = b
+            b = s
+        
+        print(f"\nThe fibonacci series with {n} elements is shown below:")
+        print(list1)
+        
+        sum = 0    #intial sum=0
+        
+        for num in list1:
+            sum = sum + num
+        average = (sum / n)
+        
+        two_decimal = "{:.2f}".format(average)
+        
+        print(f"\nAverage of given fibonacci series is {two_decimal}")
